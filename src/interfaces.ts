bfc32868822f6e28941eff78eea94a6df98b7eba
Nazeh <ar.nazeh@gmail.com>
2022-01-11 13:32:28 +0300

    fix: DHT.createServer()

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,60 +1,60 @@
-import type { Duplex, EventEmitter, Readable } from 'stream'
+import type { Duplex, EventEmitter, Readable } from 'stream';
 
 export interface KeyPair {
-  readonly publicKey: Uint8Array
-  readonly secretKey: Uint8Array
+  readonly publicKey: Uint8Array;
+  readonly secretKey: Uint8Array;
 }
 
 export interface SecretStream extends Duplex {
-  readonly publicKey: Uint8Array
-  readonly remotePublicKey: Uint8Array
-  readonly handshakeHash: Uint8Array
+  readonly publicKey: Uint8Array;
+  readonly remotePublicKey: Uint8Array;
+  readonly handshakeHash: Uint8Array;
 }
 
 export interface Server extends EventEmitter {
-  constructor: (dht: DHT, opts: any) => Server
-  listen: () => Promise<void>
+  constructor: (dht: DHT, opts: any) => Server;
+  listen: () => Promise<void>;
   address: () => {
-    host: string
-    port: number
-    publicKey: Uint8Array
-  }
-  close: () => Promise<void>
+    host: string;
+    port: number;
+    publicKey: Uint8Array;
+  };
+  close: () => Promise<void>;
   on:
-  | ((
-    event: 'connection',
-    cb: (encryptedSocket: SecretStream) => void,
-  ) => this)
-  | ((event: 'close', cb: () => void) => this)
+    | ((
+        event: 'connection',
+        cb: (encryptedSocket: SecretStream) => void,
+      ) => this)
+    | ((event: 'close', cb: () => void) => this);
 }
 
 export interface HandshakePayload {
-  isInitiator: boolean
-  publicKey: Uint8Array
-  remotePublicKey: Uint8Array
-  remoteId: Uint8Array
-  hash: Uint8Array
-  rx: Uint8Array
-  tx: Uint8Array
+  isInitiator: boolean;
+  publicKey: Uint8Array;
+  remotePublicKey: Uint8Array;
+  remoteId: Uint8Array;
+  hash: Uint8Array;
+  rx: Uint8Array;
+  tx: Uint8Array;
 }
 
 export interface ServerOpts {
-  onconnection?: Server['on']
+  onconnection?: Server['on'];
   firewall: (
     remotePublicKey: Uint8Array,
     remoteHandshakePayload: HandshakePayload,
-  ) => Promise<boolean>
+  ) => Promise<boolean>;
 }
 
 export interface DHTNode {
-  host: string
-  port: number
+  host: string;
+  port: number;
 }
 
 export interface Query extends Readable {
-  readonly from: DHTNode & { id: Uint8Array }
-  readonly to: DHTNode
-  readonly peers: [{ publickey: Uint8Array, nodes: DHTNode[] }]
+  readonly from: DHTNode & { id: Uint8Array };
+  readonly to: DHTNode;
+  readonly peers: [{ publickey: Uint8Array; nodes: DHTNode[] }];
 }
 
 export interface DHT {
@@ -64,14 +64,14 @@ export interface DHT {
    * @param {object} [opts]
    * @param {boolean} [opts.force] - force close the node without waiting for the servers to unannounce.
    */
-  destroy: (opts?: { force?: boolean }) => Promise<void>
+  destroy: (opts?: { force?: boolean }) => Promise<void>;
   /**
    * Create a new server for accepting incoming encrypted P2P connections.
    */
-  createServer:
-  | (() => Server)
-  | ((onconnection?: Server['on']) => Server)
-  | ((options?: ServerOpts, onconnection?: Server['on']) => Server)
+  createServer: (
+    options?: ServerOpts | ((encryptedSocket: SecretStream) => void),
+    onconnection?: (encryptedSocket: SecretStream) => void,
+  ) => Server;
   /**
    * @param {Uint8Array} remotePublicKey
    * @param {object} [options]
@@ -80,8 +80,8 @@ export interface DHT {
    */
   connect: (
     remotePublicKey: Uint8Array,
-    options?: { nodes: [], keyPair: KeyPair },
-  ) => SecretStream
+    options?: { nodes: []; keyPair: KeyPair },
+  ) => SecretStream;
   // TODO: add type for options.socket
   /**
    * Look for peers in the DHT on the given topic. Topic should be a 32 byte buffer (normally a hash of something).
@@ -90,11 +90,11 @@ export interface DHT {
    */
   lookup: (
     topic: Uint8Array,
-    options?: { retry: boolean, socket: any },
-  ) => Query
-  defaultKeyPair: KeyPair
+    options?: { retry: boolean; socket: any },
+  ) => Query;
+  defaultKeyPair: KeyPair;
 }
 
 export interface DHTOpts {
-  relays?: string[]
+  relays?: string[];
 }
